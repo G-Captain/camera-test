93f08d216e8c53d8f13554f80de2a47274669f90
Grzegorz Rzekanowski <grzekanowski@gmail.com>
2020-02-06 17:57:42 +0100

    Updates

---
+++ precache-manifest.7192b189efb54b62acc4c04e71552766.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddc5df78dcd180a7fa033df97bbcf250",
+    "url": "/camera-test/index.html"
+  },
+  {
+    "revision": "2218ff01f74a9fb92f4f",
+    "url": "/camera-test/static/css/main.905e5d89.chunk.css"
+  },
+  {
+    "revision": "ee9745e49e1ba98055b7",
+    "url": "/camera-test/static/js/2.bee1137f.chunk.js"
+  },
+  {
+    "revision": "8d1a37553594a39f585a245d66470763",
+    "url": "/camera-test/static/js/2.bee1137f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2218ff01f74a9fb92f4f",
+    "url": "/camera-test/static/js/main.d852c487.chunk.js"
+  },
+  {
+    "revision": "32b6b5efe85ea61b2490",
+    "url": "/camera-test/static/js/runtime-main.ff0ab6cc.js"
+  },
+  {
+    "revision": "543678af9b2229b2073ec5d8f857ab3f",
+    "url": "/camera-test/static/media/nikaragua2.543678af.jpg"
+  }
+]);
